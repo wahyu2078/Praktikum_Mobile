3bbf37a4291c7e8f4d093175c5f94009599640e3
wahyu2078 <wahyutptp20@gmail.com>
2025-09-11 17:03:07 +0700

    variabel

--- Project1/bin/pertemuan3.dart
+++ Project1/bin/pertemuan3.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:pertemuan3/pertemuan3.dart' as pertemuan3;
 
 const String nama = "Wahyu";
@@ -9,9 +10,25 @@ void main(List<String> arguments) {
   multi line comment
   banyak line comment
   */
+
   // constanta dan final
   final String nama2 = 'Wahyi'; 
 
   print("Nama saya $nama $nama2");
 
+  // variable
+  int umur = 20;
+  double tinggi = 173.5;
+  bool jenisKelamin = true; // true = laki-laki, false = perempuan
+  String alamat = "Jl. Titan 5";
+
+  stdout.writeln("Umur saya $umur");
+  stdout.writeln("Tinggi saya $tinggi");
+  stdout.writeln("Jenis kelamin saya ${jenisKelamin ? 'laki-laki' : 'perempuan'}");
+  // if (jenisKelamin) {
+  //   stdout.writeln("Jenis kelamin saya laki-laki");
+  // } else {
+  //   stdout.writeln("Jenis kelamin saya perempuan");
+  // }
+  stdout.writeln("Alamat saya $alamat");
 }
