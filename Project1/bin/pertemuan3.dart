64e084daeb0703bc35c58792698db684e912617e
wahyu2078 <wahyutptp20@gmail.com>
2025-09-11 16:59:41 +0700

    constanta dan final

--- Project1/bin/pertemuan3.dart
+++ Project1/bin/pertemuan3.dart
@@ -1,5 +1,7 @@
 import 'package:pertemuan3/pertemuan3.dart' as pertemuan3;
 
+const String nama = "Wahyu";
+
 void main(List<String> arguments) {
   // print('Hello world: ${pertemuan3.calculate()}!');
   //satu line comment
@@ -7,4 +9,9 @@ void main(List<String> arguments) {
   multi line comment
   banyak line comment
   */
+  // constanta dan final
+  final String nama2 = 'Wahyi'; 
+
+  print("Nama saya $nama $nama2");
+
 }
