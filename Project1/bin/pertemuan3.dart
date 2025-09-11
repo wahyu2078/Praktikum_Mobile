59eb04b4a51f687ca98dd6db7b66a73f60891a39
wahyu2078 <wahyutptp20@gmail.com>
2025-09-11 16:53:56 +0700

    komentar

--- Project1/bin/pertemuan3.dart
+++ Project1/bin/pertemuan3.dart
@@ -1,5 +1,10 @@
 import 'package:pertemuan3/pertemuan3.dart' as pertemuan3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${pertemuan3.calculate()}!');
+  // print('Hello world: ${pertemuan3.calculate()}!');
+  //satu line comment
+  /*
+  multi line comment
+  banyak line comment
+  */
 }
