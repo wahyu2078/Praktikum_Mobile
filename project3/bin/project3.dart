e4f495b97735966156d3c6db8f6917d408df3689
wahyu2078 <wahyutptp20@gmail.com>
2025-09-19 11:21:16 +0700

    fixed list

--- project3/bin/project3.dart
+++ project3/bin/project3.dart
@@ -1,5 +1,19 @@
+import 'dart:io';
+
 import 'package:project3/project3.dart' as project3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project3.calculate()}!');
+  // print('Hello world: ${project3.calculate()}!');
+
+  var fl = List<int>.filled(4,2);
+  List <int> angka = [1,2,3,4,5,6,7,8,9,10];
+  fl[0] = 0;
+  fl[1] = 1;
+  fl[2] = 2;
+  fl[3] = 3;
+  fl[4] = 4;
+  fl[5] = 5;
+
+  // cetak
+  stdout.writeln(fl);
 }
