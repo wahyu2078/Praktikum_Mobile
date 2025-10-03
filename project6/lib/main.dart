560302bda70f2449b2fd3256cca875ff09673676
wahyu2078 <wahyutptp20@gmail.com>
2025-10-03 10:46:26 +0700

    dasar drawer

--- project6/lib/main.dart
+++ project6/lib/main.dart
@@ -15,19 +15,36 @@ class MyApp extends StatelessWidget {
           title: const Text('Hello World'),
           backgroundColor: Colors.blue,
           leading: const Icon(Icons.menu),
-          actions: const [
-            Icon(Icons.search),
-            Icon(Icons.more_vert),
-          ],
+          actions: const [Icon(Icons.search), Icon(Icons.more_vert)],
         ),
-        body: const Center(
-          child: Text('Hello World'),
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+              ListTile(leading: Icon(Icons.message), title: Text('Messages')),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
+            ],
+          ),
         ),
+
+        body: const Center(child: Text('Hello World')),
         floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
           onPressed: () {},
+          child: const Icon(Icons.add),
         ),
-      )
+      ),
     );
   }
 }
