da2b8aef35c84ee70819b97bc68141c7c34f0f90
wahyu2078 <wahyutptp20@gmail.com>
2025-10-03 10:11:35 +0700

    konten body dengan styling

--- project4/lib/main.dart
+++ project4/lib/main.dart
@@ -1,33 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(WahyuApp());
+  runApp(LuqmanApp());
 }
 
-class WahyuApp extends StatelessWidget {
+class LuqmanApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Luqman App",
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            'Wahyu Demo',
+            "Balonku ada 5",
             style: TextStyle(
-              fontSize: 20,
+              fontSize: 24,
               fontWeight: FontWeight.bold,
-              color: Colors.purple,
-              fontFamily: 'Roboto',
+              color: Colors.white,
+              fontFamily: 'Arial',
             ),
           ),
-          backgroundColor: Colors.red,
-          centerTitle: true,
+          backgroundColor: Colors.blue,
+          centerTitle: false,
           foregroundColor: Colors.white,
         ),
         body: Column(
-          
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Balonku ada 5\nRupa-rupa warnanya\nHijau, kuning, kelabu\nMerah muda dan biru",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontFamily: 'Arial',
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Meletus balon hijau\nHatiku sangat kacau\nBalonku tinggal 4\nKupegang erat-erat",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey[700],
+                fontFamily: 'Arial',
+              ),
+            ),
+          ],
         ),
-      ),
-    );
-  }
+     ),
+  );
+}
 }
