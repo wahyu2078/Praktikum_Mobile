88116a51f818436a409425aa40808407ff156418
wahyu2078 <wahyutptp20@gmail.com>
2025-10-08 20:34:39 +0700

    navigasi antar halaman

--- tugaspertemuan6/lib/main.dart
+++ tugaspertemuan6/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'pages/counter_page.dart'; 
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/profile_page.dart';
+import 'pages/counter_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,18 +13,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorScheme = ColorScheme.fromSeed(
-      seedColor: const Color(0xFF0F766E),
+      seedColor: const Color(0xFF0F766E), // teal-ish
       brightness: Brightness.light,
     );
 
     return MaterialApp(
-      title: 'tugaspertemuan6',
+      title: 'My Profile & Counter App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: colorScheme,
         useMaterial3: true,
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      home: const CounterPage(),
+      home: const HomeShell(),
+    );
+  }
+}
+
+class HomeShell extends StatefulWidget {
+  const HomeShell({super.key});
+
+  @override
+  State<HomeShell> createState() => _HomeShellState();
+}
+
+class _HomeShellState extends State<HomeShell> {
+  int _index = 0;
+
+  final List<Widget> _pages = const [
+    ProfilePage(), // StatelessWidget
+    CounterPage(), // StatefulWidget
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // Root Scaffold hanya untuk bottom navigation; tiap halaman punya Scaffold + AppBar sendiri
+    return Scaffold(
+      body: IndexedStack(
+        index: _index,
+        children: _pages,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _index,
+        onDestinationSelected: (i) => setState(() => _index = i),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.calculate_outlined),
+            selectedIcon: Icon(Icons.calculate),
+            label: 'Counter',
+          ),
+        ],
+      ),
     );
   }
 }
