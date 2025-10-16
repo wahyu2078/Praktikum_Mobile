bf5eecf1ee3e8f94a34e7b922ee041f188ad7e12
wahyu2078 <wahyutptp20@gmail.com>
2025-10-17 05:39:50 +0700

    langkah-langkah single child

--- project7/lib/main.dart
+++ project7/lib/main.dart
@@ -1,56 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const GantiWarnaApp());
+  runApp(const SingleChildExample());
 }
 
-class GantiWarnaApp extends StatefulWidget {
-  const GantiWarnaApp({super.key});
-
-  @override
-  State<GantiWarnaApp> createState() => _GantiWarnaAppState();
-}
-
-class _GantiWarnaAppState extends State<GantiWarnaApp> {
-  bool _isBlue = true;
-
-  void _ubahWarna() {
-    setState(() {
-      _isBlue = !_isBlue;
-    });
-  }
+class SingleChildExample extends StatelessWidget {
+  const SingleChildExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Contoh Stateful: Ubah Warna',
+      title: 'Single Child Layout Example',
       home: Scaffold(
-        backgroundColor: _isBlue ? Colors.blue[100] : Colors.pink[100],
         appBar: AppBar(
-          title: const Text('Contoh Stateful: Ubah Warna'),
-          backgroundColor: _isBlue ? Colors.blue : Colors.pink,
+          title: const Text('Single Child Layout Example'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                _isBlue ? 'Warna Biru' : 'Warna Merah Muda',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: _isBlue ? Colors.blue[900] : Colors.pink[900],
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _ubahWarna,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: _isBlue ? Colors.blue : Colors.pink,
-                ),
-                child: const Text('Ubah Warna'),
-              ),
-            ],
+          child: Container(
+            padding: const EdgeInsets.all(30),
+            color: Colors.lightGreen,
+            child: const Text(
+              'Hello Flutter!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
           ),
         ),
       ),
